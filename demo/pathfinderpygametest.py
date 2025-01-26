98b4cad3391d74e85d2b0aeb4228e03604871473
PainDeMie64 <63594296+PainDeMie64@users.noreply.github.com>
2025-01-27 00:11:28 +0100

    Performance improvements and general refactor

--- demo/pathfinderpygametest.py
+++ demo/pathfinderpygametest.py
@@ -1,13 +1,8 @@
-import math
 import numpy as np
 import numba
+from numba.typed import List
 import rustworkx as rx
 from shapely.geometry import Polygon
-
-import numpy as np
-import numba
-import math
-from numba.typed import List
 
 class CollisionChecker:
     def __init__(self, obstacles, player_radius):
@@ -17,24 +12,37 @@ class CollisionChecker:
     def update_obstacles(self, collision_obstacles):
         self.collision_polygons = List()
         self.collision_aabbs = List()
+        self.collision_edge_aabbs = List()
         for poly in collision_obstacles:
-            poly_np = np.array(poly, dtype=np.float64)
-            min_x = np.min(poly_np[:, 0])
-            max_x = np.max(poly_np[:, 0])
-            min_y = np.min(poly_np[:, 1])
-            max_y = np.max(poly_np[:, 1])
-            self.collision_polygons.append(poly_np)
+            min_x = np.min(poly[:, 0])
+            max_x = np.max(poly[:, 0])
+            min_y = np.min(poly[:, 1])
+            max_y = np.max(poly[:, 1])
+            self.collision_polygons.append(poly)
             self.collision_aabbs.append(np.array([min_x, min_y, max_x, max_y], dtype=np.float64))
+
+            edge_aabbs = []
+            n = poly.shape[0]
+            for j in range(n):
+                a = poly[j]
+                b = poly[(j+1) % n]
+                edge_aabbs.append([
+                    min(a[0], b[0]),
+                    min(a[1], b[1]),
+                    max(a[0], b[0]),
+                    max(a[1], b[1])
+                ])
+            self.collision_edge_aabbs.append(np.array(edge_aabbs, dtype=np.float64))
     
     def line_intersects_with_any_obstacle(self, p1, p2):
         p1_np = np.array(p1, dtype=np.float64)
         p2_np = np.array(p2, dtype=np.float64)
-        return intersects_check(p1_np, p2_np, self.collision_polygons, self.collision_aabbs)
+        return intersects_check(p1_np, p2_np, self.collision_polygons, self.collision_aabbs, self.collision_edge_aabbs)
     
     def point_inside_obstacle(self, point):
-        point_np = np.array([[point[0], point[1]]], dtype=np.float64)
+        point_np = np.array([point[0], point[1]], dtype=np.float64)
         for poly in self.collision_obstacles:
-            if is_inside_sm_parallel(point_np, poly)[0]:
+            if is_inside_sm(poly, point_np):
                 return True
         return False
     
@@ -46,9 +54,9 @@ class CollisionChecker:
             exterior = np.array(buffered_poly.exterior.coords[:-1], dtype=np.float64)
             buffered.append(exterior)
         return buffered
-
-@numba.njit
-def intersects_check(p1, p2, collision_polygons, collision_aabbs):
+    
+@numba.njit(fastmath=True)
+def intersects_check(p1, p2, collision_polygons, collision_aabbs, edge_aabbs):
     min_x_seg = min(p1[0], p2[0])
     max_x_seg = max(p1[0], p2[0])
     min_y_seg = min(p1[1], p2[1])
@@ -64,15 +72,13 @@ def intersects_check(p1, p2, collision_polygons, collision_aabbs):
                 return True
         n = poly.shape[0]
         for j in range(n):
-            a = poly[j]
-            b = poly[(j+1) % n]
-            min_x_edge = min(a[0], b[0])
-            max_x_edge = max(a[0], b[0])
-            min_y_edge = min(a[1], b[1])
-            max_y_edge = max(a[1], b[1])
+            min_x_edge, min_y_edge, max_x_edge, max_y_edge = edge_aabbs[i][j]
+            
             if (min_x_edge > max_x_seg) or (max_x_edge < min_x_seg) or (min_y_edge > max_y_seg) or (max_y_edge < min_y_seg):
                 continue
             
+            a = poly[j]
+            b = poly[(j+1) % n]
             if segments_intersect(p1, p2, a, b):
                 return True
     
@@ -132,18 +138,24 @@ def get_weight(p1, p2):
     return ((p1[0]-p2[0])**2+(p1[1]-p2[1])**2)**0.5
 
 class Pathfinder:
-    def __init__(self, obstacles, player_radius):
+    def __init__(self, obstacles, player_radius, collision_offset=-1, path_offset=2):
         self.obstacles = [np.array(obs, dtype=np.float64) for obs in obstacles]
-        self.collision_checker = CollisionChecker(self.obstacles, player_radius + 1)
-        self.path_obstacles = self.collision_checker.create_buffered_obstacles(self.obstacles, player_radius + 2)
+        self.collision_checker = CollisionChecker(self.obstacles, player_radius+collision_offset)
+        self.path_obstacles = self.collision_checker.create_buffered_obstacles(self.obstacles, player_radius + path_offset)
         self.player_radius = player_radius
         self.passable_points = self._generate_passable_points()
         self.concatenated_passable_points = self._generate_concatenated_passable_points()
         self.graph = rx.PyGraph()
         self._build_base_graph()
+
+        self.width_interval = 0
+        self.height_interval = 0
+        self.preprocessed_points = []
+        self.preprocessed_paths = dict()
+        self.last_path = []
     
     def _generate_passable_points(self):
-        return [obstacle.tolist() for obstacle in self.path_obstacles]
+        return [obstacle for obstacle in self.path_obstacles]
     
     def _generate_concatenated_passable_points(self):
         return [tuple(point) for obstacle in self.passable_points for point in obstacle]
@@ -171,40 +183,108 @@ class Pathfinder:
         start_node_idx = temp_graph.add_node(start_pos)
         end_node_idx = temp_graph.add_node(end_pos)
         
-        for i in range(n_original):
-            point = points[i]
-            if not self.collision_checker.line_intersects_with_any_obstacle(start_pos, point):
-                weight = get_weight(start_pos, point) * self._speed_multiplier(start_pos, point)
-                temp_graph.add_edge(start_node_idx, i, weight)
-            if not self.collision_checker.line_intersects_with_any_obstacle(end_pos, point):
-                weight = get_weight(end_pos, point) * self._speed_multiplier(end_pos, point)
-                temp_graph.add_edge(end_node_idx, i, weight)
-        
         if not self.collision_checker.line_intersects_with_any_obstacle(start_pos, end_pos):
             weight = get_weight(start_pos, end_pos) * self._speed_multiplier(start_pos, end_pos)
             temp_graph.add_edge(start_node_idx, end_node_idx, weight)
+        else:
+            for i in range(n_original):
+                point = points[i]
+                if not self.collision_checker.line_intersects_with_any_obstacle(start_pos, point):
+                    weight = get_weight(start_pos, point) * self._speed_multiplier(start_pos, point)
+                    temp_graph.add_edge(start_node_idx, i, weight)
+                if not self.collision_checker.line_intersects_with_any_obstacle(end_pos, point):
+                    weight = get_weight(end_pos, point) * self._speed_multiplier(end_pos, point)
+                    temp_graph.add_edge(end_node_idx, i, weight)
         
-        path_indices = rx.astar_shortest_path(
-            temp_graph,
-            start_node_idx,
-            goal_fn=lambda data: data == end_pos,
-            edge_cost_fn=lambda e: e,
-            estimate_cost_fn=lambda pos: get_weight(pos, end_pos)
-        )
-        if path_indices is None:
-            return None
-        path_points = [temp_graph[node_idx] for node_idx in path_indices]
-        return path_points
-    
+        try:
+            path_indices = rx.astar_shortest_path(
+                temp_graph,
+                start_node_idx,
+                goal_fn=lambda data: data == end_pos,
+                edge_cost_fn=lambda e: e,
+                estimate_cost_fn=lambda pos: get_weight(pos, end_pos)
+            )
+            if path_indices is None:
+                return None
+            path_points = [temp_graph[node_idx] for node_idx in path_indices]
+            return path_points
+        except rx.NoPathFound:
+            return self.last_path
+        
     def get_move_direction(self, current_pos, target_pos):
-        path = self.find_path(current_pos, target_pos)
-        if not path or len(path) < 2:
-            return (0.0, 0.0)
+        if(len(self.preprocessed_paths)==0):
+            path = self.find_path(current_pos, target_pos)
+        else:
+            path = self.find_prepocessed_path(current_pos, target_pos)
+        if path is None or len(path) < 2:
+            return (0, 0)
+        self.last_path = path
         next_point = path[1]
         dx = next_point[0] - current_pos[0]
         dy = next_point[1] - current_pos[1]
         return (dx, dy)
+    
+    # def find_prepocessed_path(self, start_pos, end_pos):
+    #     start_x_idx, start_y_idx = int(start_pos[0]/self.width_interval), int(start_pos[1]/self.height_interval)
+    #     end_x_idx, end_y_idx = int(end_pos[0]/self.width_interval), int(end_pos[1]/self.height_interval)
+        
+    #     if start_x_idx+1>=len(self.preprocessed_points) or start_y_idx+1>=len(self.preprocessed_points) or end_x_idx+1>=len(self.preprocessed_points) or end_y_idx+1>=len(self.preprocessed_points):
+    #         print("Error: Preprocessed points are not enough")
+    #         return self.find_path(start_pos, end_pos)
+    #     start_corners=[(start_x_idx*self.width_interval, start_y_idx*self.height_interval),
+    #                       ((start_x_idx+1)*self.width_interval, start_y_idx*self.height_interval),
+    #                       (start_x_idx*self.width_interval, (start_y_idx+1)*self.height_interval),
+    #                       ((start_x_idx+1)*self.width_interval, (start_y_idx+1)*self.height_interval)]
+    #     end_corners=[(end_x_idx*self.width_interval, end_y_idx*self.height_interval),
+    #                         ((end_x_idx+1)*self.width_interval, end_y_idx*self.height_interval),
+    #                         (end_x_idx*self.width_interval, (end_y_idx+1)*self.height_interval),
+    #                         ((end_x_idx+1)*self.width_interval, (end_y_idx+1)*self.height_interval)]
+    #     for corner in start_corners:
+    #         if not self.collision_checker.line_intersects_with_any_obstacle(start_pos, corner):
+    #             reachable_start_point=corner
+    #             break
+    #     else:
+    #         return self.find_path(start_pos, end_pos)
+    #     for corner in end_corners:
+    #         if not self.collision_checker.line_intersects_with_any_obstacle(end_pos, corner):
+    #             reachable_end_point=corner
+    #             break
+    #     else:
+    #         return self.find_path(start_pos, end_pos)
+    #     if reachable_start_point==reachable_end_point:
+    #         return self.find_path(start_pos, end_pos)
+    #     try:
+    #         closest_path=self.preprocessed_paths[(reachable_start_point, reachable_end_point)]
+    #     except KeyError:
+    #         closest_path=self.preprocessed_paths[(reachable_end_point, reachable_start_point)][::-1]
+    #     return [start_pos]+closest_path[1:-1]+[end_pos]
+    
 
+    # def preprocess_paths(self, resolution_x, resolution_y, width, height):
+    #     self.width_interval = width / resolution_x
+    #     self.height_interval = height / resolution_y
+    #     self.preprocessed_points = []
+    #     for i in range(resolution_x):
+    #         x = i * self.width_interval
+    #         for j in range(resolution_y):
+    #             y = j * self.height_interval
+    #             point = (x, y)
+    #             self.preprocessed_points.append(point)
+    #     self.preprocessed_paths = {}
+    #     prog = 0
+    #     total = len(self.preprocessed_points) * (len(self.preprocessed_points) - 1) / 2
+    #     for i, point in enumerate(self.preprocessed_points):
+    #         if i%10==0:
+    #             print(f"Progress: {prog}/{total}")
+    #         for j in range(i + 1, len(self.preprocessed_points)):
+    #             prog+=1
+    #             target_point = self.preprocessed_points[j]
+    #             try:
+    #                 path = self.find_path(point, target_point)
+    #                 if path is not None:
+    #                     self.preprocessed_paths[(point, target_point)] = path
+    #             except rx.NoPathFound:
+    #                 continue
 
 
 import pygame
@@ -270,13 +350,6 @@ class Goal:
         self.x = random.randint(50, WIDTH-50)
         self.y = random.randint(50, HEIGHT-50)
 
-pathfinder = Pathfinder(obstacles, player_radius=PLAYER_SIZE) 
-def test_get_move_direction():
-    for _ in range(1000):
-        current_pos = (653, 319)
-        target_pos = (105, 542)
-        pathfinder.get_move_direction(current_pos, target_pos)
-import cProfile
 def main():
     running = True
     clock = pygame.time.Clock()
@@ -285,14 +358,16 @@ def main():
     goal = Goal()
     iteration=0
     
+    pathfinder = Pathfinder(obstacles, player_radius=PLAYER_SIZE) 
+    # pathfinder.preprocess_paths(20, 20, WIDTH, HEIGHT)
     collision_checker = CollisionChecker(obstacles, player_radius=PLAYER_SIZE+2)
     
     while running:
         iteration+=1
         
-        if iteration%200==0:
-            # print(timeit.timeit(lambda: pathfinder.get_move_direction((player.x,player.y), (goal.x,goal.y)), number=1000)/1000)
-            cProfile.run('test_get_move_direction()', sort='cumtime')
+        # if iteration%200==0:
+        #     print(timeit.timeit(lambda: pathfinder.get_move_direction((player.x,player.y), (goal.x,goal.y)), number=1000)/1000)
+            
         screen.fill(WHITE)
 
         # Draw obstacles
